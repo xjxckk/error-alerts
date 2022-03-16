7635a88d608c03ca1dd5e1d7960ba8e33854365f
xJxckk <17519346+xjxckk@users.noreply.github.com>
2022-03-16 16:38:54 +0000

    update

--- alerts/__init__.py
+++ alerts/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import traceback, telegram
 
 class setup:
@@ -16,23 +15,4 @@ class setup:
         print(message)
         print()
         if self.raise_error:
-=======
-import traceback, telegram
-
-class setup:
-    def __init__(self, telegram_token, telegram_channel, full_error=False, raise_error=False):
-        self.bot = telegram.Bot(token=telegram_token)
-        self.channel = telegram_channel
-        self.full_error = full_error
-        self.raise_error = raise_error
-    def send(self, title, exception):
-        error = str(exception)
-        if self.full_error:
-            error = traceback.format_exc()
-        message = f'{title}: {error}'
-        self.bot.send_message(self.channel, message)
-        print(message)
-        print()
-        if self.raise_error:
->>>>>>> 8b495d93b88c3bead1b5685a1069d23dfc62037b
             raise Exception('Raiser') from exception
--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from setuptools import setup
 
 setup(
@@ -10,17 +9,4 @@ setup(
     download_url = 'https://github.com/xjxckk/error-alerts/archive/refs/tags/v2.tar.gz',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown'
-=======
-from setuptools import setup
-
-setup(
-    name = 'error-alerts',
-    packages = ['alerts'],
-    version = '1',
-    description = 'Error alerts via Telegram',
-    url = 'https://github.com/xjxckk/error-alerts/',
-    download_url = 'https://github.com/xjxckk/error-alerts/archive/refs/tags/v1.tar.gz',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown'
->>>>>>> 8b495d93b88c3bead1b5685a1069d23dfc62037b
     )
