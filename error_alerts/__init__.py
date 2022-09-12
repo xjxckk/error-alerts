5a9dd8f06851961bd7fc6640011be6c186d0b83e
xJxckk <17519346+xjxckk@users.noreply.github.com>
2022-09-12 11:04:33 +0100

    handle telegram servers being down

---
+++ error_alerts/__init__.py
@@ -0,0 +1,39 @@
+import traceback
+from telegram import Bot
+
+class setup:
+    def __init__(self, telegram_token, telegram_channel, full_error=False, raise_error=False, resend_repeat_errors=True):
+        self.bot = Bot(token=telegram_token)
+        self.channel = telegram_channel
+        self.full_error = full_error
+        self.raise_error = raise_error
+        self.resend_repeat_errors = resend_repeat_errors
+        self.last_error = None
+    def send(self, title, exception):
+        error = str(exception)
+        if self.full_error:
+            error = traceback.format_exc()
+        message = f'{title}: {error}'
+        print(message)
+        print()
+        if self.resend_repeat_errors or error != self.last_error:
+            try:
+                self.bot.send_message(self.channel, message[:4096])
+                self.last_error = error
+            except Exception as telegram_error:
+                print('Error sending alert message to Telegram:', telegram_error)
+                print()
+        if self.raise_error:
+            raise Exception('Raiser') from exception
+    def send_message(self, *messages):
+        final_message = ''
+        for message in messages:
+            final_message += message
+            final_message += ' '
+        print(final_message)
+        print()
+        try:
+            self.bot.send_message(self.channel, final_message)
+        except Exception as telegram_error:
+            print('Error sending message to Telegram:', telegram_error)
+            print()
--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name = 'error-alerts',
-    packages = ['error-alerts'],
-    version = '2',
+    packages = ['error_alerts'],
+    version = '2.7',
     description = 'Error alerts via Telegram',
     url = 'https://github.com/xjxckk/error-alerts/',
     download_url = 'https://github.com/xjxckk/error-alerts/archive/refs/tags/v2.tar.gz',
     long_description=open('README.md').read(),
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    install_requires=['python-telegram-bot']
     )
